b5b72ed5baa9acd08c21b6f0faf5d4a65a44cef6
kurkune <136829248+kurkune@users.noreply.github.com>
2023-06-23 17:17:06 +0300

    Add files via upload

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -9,7 +9,9 @@ void cg::renderer::rasterization_renderer::init()
 	rasterizer->set_viewport(settings->width, settings->height);
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 			settings->width,settings->height );
-	rasterizer ->set_render_target(render_target);
+	depth_buffer = std::make_shared<cg::resource<float>>(
+			settings->width,settings->height );
+	rasterizer ->set_render_target(render_target, depth_buffer);
 	model = std::make_shared<cg::world::model>();
 	model -> load_obj(settings -> model_path);
 
@@ -27,7 +29,7 @@ void cg::renderer::rasterization_renderer::init()
 	camera->set_angle_of_view(settings->camera_angle_of_view);
 	camera->set_z_near(settings->camera_z_near);
 	camera->set_z_far(settings->camera_z_far);
-	// TODO Lab: 1.06 Add depth buffer in `cg::renderer::rasterization_renderer`
+
 }
 void cg::renderer::rasterization_renderer::render()
 {
@@ -64,8 +66,8 @@ void cg::renderer::rasterization_renderer::render()
 	std::cout<<"Rasterization took "<<duration.count()<<" ms\n";
 	cg::utils::save_resource(*render_target, settings->result_path);
 
-	// TODO Lab: 1.04 Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
-	// TODO Lab: 1.05 Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
+
+
 
 }
 
